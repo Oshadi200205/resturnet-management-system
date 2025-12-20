f0c37ec01bdc5970558614eb3e5735fb50385e5c
oshadi200205 <nawanjanaoshadi24@outlook.com>
2025-12-20 09:32:38 +0530

    first commit

---
+++ src/test/java/edu/rsm/RsmApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.rsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
